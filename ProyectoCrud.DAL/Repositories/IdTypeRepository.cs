d5b46e3bd45ad099bd47d02d5a95b979e211b130
Oswal Noguera <126040008+OswalNo15@users.noreply.github.com>
2024-02-15 23:11:41 -0500

    anadiendo repositorios

---
+++ ProyectoCrud.DAL/Repositories/IdTypeRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoCrud.DAL.BD_Context;
+using ProyectoCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCrud.DAL.Repositories
+{
+    public class IdTypeRepository : IGenericRepository<IdType>
+    {
+        private readonly BdMilesCarRentalContext _context;
+        //constructor
+        public IdTypeRepository(BdMilesCarRentalContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> Delete(int id)
+        {
+            try
+            {
+                User model = _context.Users.First(c => c.IdUser == id.ToString());
+                _context.Users.Remove(model);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+       
+        }
+
+        public async Task<IdType> Get(int id)
+        {
+            return await _context.IdTypes.FindAsync(id).ConfigureAwait(false);
+        }
+
+        public async Task<IQueryable<IdType>> GetAll()
+        {
+            IQueryable<IdType> queryContratoSQL = _context.IdTypes;
+            return queryContratoSQL;
+        }
+
+        public async Task<bool> Insert(IdType model)
+        {
+            _context.Add(model);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Update(IdType model)
+        {
+            _context.IdTypes.Update(model);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ ProyectoCrud.DAL/Repositories/PreferenceClientRepository.cs
@@ -0,0 +1,54 @@
+﻿using ProyectoCrud.DAL.BD_Context;
+using ProyectoCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCrud.DAL.Repositories
+{
+    public  class PreferenceClientRepository : IGenericRepository<PreferenceClient>
+    {
+
+        private readonly BdMilesCarRentalContext _context;
+        //constructor
+        public  PreferenceClientRepository(BdMilesCarRentalContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            PreferenceClient model = _context.PreferenceClients.First(c => c.IdPreferenceType == id);
+            _context.PreferenceClients.Remove(model);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<PreferenceClient> Get(int id)
+        {
+            return await _context.PreferenceClients.FindAsync(id).ConfigureAwait(false);
+        }
+
+        public async Task<IQueryable<PreferenceClient>> GetAll()
+        {
+            IQueryable<PreferenceClient> queryContratoSQL = _context.PreferenceClients;
+            return queryContratoSQL;
+        }
+
+        public async Task<bool> Insert(PreferenceClient model)
+        {
+            _context.Add(model);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Update(PreferenceClient model)
+        {
+            _context.PreferenceClients.Update(model);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
