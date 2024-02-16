5e4e1e5488a1cb4897ef03b34ed13cf038d818eb
OswalN <cerocinco9@gmail.com>
2024-02-15 23:29:43 -0500

    ajuste finales del jwt

--- CarRent/Program.cs
+++ CarRent/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using ProyectoCrud.DAL;
 using ProyectoCrud.DAL.BD_Context;
+using ProyectoCrud.DAL.Services;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +23,35 @@ builder.Services.AddDbContext<BdMilesCarRentalContext>(opt => opt.UseSqlServer(b
 #endregion
 
 
+#region Inyeccion de dependencias
+builder.Services.AddScoped<IAuthorizationService, AuthorizationService>();
+#endregion
+
+#region configuration of JTW
+var key = builder.Configuration.GetValue<string>("JWTSettings:key");
+var keyBytes = Encoding.ASCII.GetBytes(key);
+
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+    };
+
+});
+
+#endregion
 
 var app = builder.Build();
 
@@ -27,9 +64,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
 app.UseHttpsRedirection();
-
+app.MapControllers();
 
 app.Run();
 
