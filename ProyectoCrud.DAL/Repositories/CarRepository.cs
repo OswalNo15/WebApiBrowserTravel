588ca15daf3da7a60d8398a40f681f47f72f6a99
OswalN <cerocinco9@gmail.com>
2024-02-15 22:46:18 -0500

    Repository interfaces

---
+++ ProyectoCrud.DAL/Repositories/CarRepository.cs
@@ -0,0 +1,47 @@
+﻿using ProyectoCrud.DAL.BD_Context;
+using ProyectoCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCrud.DAL.Repositories
+{
+    public class CarRepository : IGenericRepository<Car>
+    {
+        private readonly BdMilesCarRentalContext _context;
+        //constructor
+        public CarRepository(BdMilesCarRentalContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> Delete(int id)
+        {
+            Car model = _context.Cars.First(c => c.IdCar == id);
+            _context.Cars.Remove(model);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<Car> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IQueryable<Car>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Insert(Car model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Update(Car model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProyectoCrud.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using ProyectoCrud.DAL.BD_Context;
+using ProyectoCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCrud.DAL.Repositories
+{
+    public class UserRepository : IGenericRepository<User>
+    {
+        private readonly BdMilesCarRentalContext _context;
+        //constructor
+        public UserRepository(BdMilesCarRentalContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> Delete(int id)
+        {
+            User model = _context.Users.First(c => c.IdUser == id.ToString());
+            _context.Users.Remove(model);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<User> Get(int id)
+        {
+            return await _context.Users.FindAsync(id).ConfigureAwait(false);
+        }
+
+        public async Task<IQueryable<User>> GetAll()
+        {
+            IQueryable<User> queryContratoSQL = _context.Users;
+            return queryContratoSQL;
+        }
+
+        public async Task<bool> Insert(User model)
+        {
+            _context.Add(model);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Update(User model)
+        {
+            _context.Users.Update(model);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
