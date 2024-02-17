d132ce85d67cf6d45aad55d527f38a70fadea1e5
Oswal Noguera <126040008+OswalNo15@users.noreply.github.com>
2024-02-16 22:37:07 -0500

    creando otro controlador y la interfaz de servicio de preference

---
+++ CarRent/Controllers/PreferenceController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoCrud.BLL.Service;
+using System.ComponentModel.Design;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CarRent.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PreferenceController : ControllerBase
+    {
+
+        private readonly IAuthorizationService _authorizationService;
+
+        private readonly IPreferenceService _PreferenceService;
+
+
+        public PreferenceController(IReferenceService PreferenceService)
+        {
+            this._PreferenceService = PreferenceService;
+        }
+
+        public PreferenceController(IAuthorizationService authorizationService)
+        {
+            _authorizationService = authorizationService;
+        }
+
+        // GET: api/<PreferenceController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PreferenceController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PreferenceController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PreferenceController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PreferenceController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ProyectoCrud.BLL/Service/Preferenceservice.cs
@@ -0,0 +1,44 @@
+﻿using ProyectoCrud.DAL.Repositories;
+using ProyectoCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCrud.BLL.Service
+{
+    internal class Preferenceservice : IPreference
+    {
+        private readonly IGenericRepository<Preference> _PreferenceRepo;
+        public Preferenceservice(IGenericRepository<Preference> PreferenceRepo)
+        {
+            _PreferenceRepo = PreferenceRepo;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            return await _PreferenceRepo.Delete(id);
+        }
+
+        public Task<Preference> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<Preference>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Insert(Preference model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Preference model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
